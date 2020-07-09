aae3ff395218acb45158bda1c3c1823ef2c05248
Edgar Marques <edgarmarques.110@gmail.com>
2020-07-09 11:34:42 -0300

    Add base code

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <section className="app" />
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
